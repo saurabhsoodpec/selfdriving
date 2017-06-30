bd12543666bc445e9bc5b25889404bd8659a3e9f
Saurabh Sood <saurabh.pec@gmail.com>
2017-06-30 01:31:55 -0700

    More code optimizations and parameter tuning.

--- CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -40,13 +40,13 @@ UKF::UKF() {
   std_laspy_ = 0.15;
 
   // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.5;
+  std_radr_ = 0.3;
 
   // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.01;
+  std_radphi_ = 0.03;
 
   // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.25;
+  std_radrd_ = 0.3;
 
   /**
   TODO:
